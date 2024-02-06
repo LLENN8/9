bac88e6a1b4f0151ae0dbe7e9a62b58bd5b79893
LLENN8 <handhikaprasetya74@gmail.com>
2024-02-06 12:00:29 +0700

    fixing error endless loading when convert

--- setup.py
+++ setup.py
@@ -5,5 +5,5 @@ setup(
     name="Misa Converter",
     version="0.1",
     description="Convert mp4 to mp3",
-    executables=[Executable("Misa_Converter.py",  target_name="Misa Converter.exe", base="Win32GUI")]
+    executables=[Executable("Misa_Converter.py",  target_name="Misa Converter.exe", base=None)]
 )
