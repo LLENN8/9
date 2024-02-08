885b8315f91b5445850f0071dd31f111f5c01322
LLENN8 <handhikaprasetya74@gmail.com>
2024-02-08 11:29:07 +0700

    feature: Hidden Console when converting

--- converter.py
+++ converter.py
@@ -10,40 +10,41 @@ class Converter:
             if selected_conversion == "MP4 to MP3":
                 output_extension = ".mp3"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, format='mp3', codec='copy').run()
+                ffmpeg.input(input_file).output(output_path, format='mp3', codec='copy', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "MKV to MP3":
                 output_extension = ".mp3"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, format='mp3', codec='copy').run()
+                ffmpeg.input(input_file).output(output_path, format='mp3', codec='copy', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "AVI to MP3":
                 output_extension = ".mp3"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, format='mp3', codec='copy').run()
+                ffmpeg.input(input_file).output(output_path, format='mp3', codec='copy', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "MP4 to MKV":
                 output_extension = ".mkv"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, codec='copy').run()
+                ffmpeg.input(input_file).output(output_path, codec='copy', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "MKV to MP4":
                 output_extension = ".mp4"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, codec='copy').run()
+                ffmpeg.input(input_file).output(output_path, codec='copy', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "AVI to MP4":
                 output_extension = ".mp4"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, codec='copy').run()
+                ffmpeg.input(input_file).output(output_path, codec='copy', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "MP4 to AVI":
                 output_extension = ".avi"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, vcodec='libx264', acodec='mp3').run()
+                ffmpeg.input(input_file).output(output_path, vcodec='libx264', acodec='mp3', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "MKV to AVI":
                 output_extension = ".avi"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, vcodec='libx264', acodec='mp3').run()
+                ffmpeg.input(input_file).output(output_path, vcodec='libx264', acodec='mp3', y='-y').run(capture_stdout=True, capture_stderr=True)
             elif selected_conversion == "AVI to MKV":
                 output_extension = ".mkv"
                 output_path = os.path.join(output_folder, output_file + output_extension)
-                ffmpeg.input(input_file).output(output_path, vcodec='libx264', acodec='mp3').run()
+                ffmpeg.input(input_file).output(output_path, vcodec='libx264', acodec='mp3', y='-y').run(capture_stdout=True, capture_stderr=True)
             
-            progress_label.config(text=f"Konversi: {i}/{total_files}")
-            root.update()
+            if progress_label.winfo_exists():
+                progress_label.config(text=f"Konversi: {i}/{total_files}")
+                root.update()
 
